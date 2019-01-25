9b86ec8f96d38b3759ab51c4eff720da3e9d51d7
bogdaner2 <KonorinDeveloper@gmail.com>
2019-01-25 10:52:32 +0200

    Created ui and logic of calculations

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
-app.get('/', (request, response) => {
-    response.send('New server')
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('New server');
 })
+
+app.get('/fibonacci', (req, res) => {
+    res.sendFile(path.join(__dirname+'/Views/index.html'));
+})
+
+app.post('/fibonacci', (req, res) => {
+    res.send(JSON.stringify(fibonacci(req.body.number)));
+})
+
 app.listen(port, (err) => {
     if (err) {
         return console.log(err)
     }
     console.log(`server is listening on ${port}`)
 })
+
+const fibonacci = (n) => n > 1 ? (fibonacci(n - 1) + fibonacci(n - 2)) : 1;
