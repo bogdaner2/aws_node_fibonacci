5393d3a6ca451816085a18bdfb970aa831bbffaf
bogdaner2 <KonorinDeveloper@gmail.com>
2019-01-25 12:17:36 +0200

    Added store in Amazon S3

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const aws = require('aws-sdk');
+const s3 = new aws.S3({
+    accessKeyId: process.env.USER_KEY,
+    secretAccessKey: process.env.USER_SECRET,
+});
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('New server');
+    res.send('Go to /fibonacci for calculations!!!');
 })
 
 app.get('/fibonacci', (req, res) => {
@@ -14,14 +20,30 @@ app.get('/fibonacci', (req, res) => {
 })
 
 app.post('/fibonacci', (req, res) => {
-    res.send(JSON.stringify(fibonacci(req.body.number)));
+
+    const resFib = fibonacci(req.body.number);
+
+    const params = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: 'result.json',
+        Body: JSON.stringify({key: req.body.number, res : resFib})
+    }
+
+    s3.upload(params, (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+        }
+    );
+
+    res.send(JSON.stringify(resFib));
 })
 
-app.listen(port, (err) => {
+app.listen(process.env.PORT, (err) => {
     if (err) {
         return console.log(err)
     }
-    console.log(`server is listening on ${port}`)
+    console.log(`server is listening on ${process.env.PORT}`)
 })
 
-const fibonacci = (n) => n > 1 ? (fibonacci(n - 1) + fibonacci(n - 2)) : 1;
+const fibonacci = (n) => n > 2 ? (fibonacci(n - 1) + fibonacci(n - 2)) : 1;
